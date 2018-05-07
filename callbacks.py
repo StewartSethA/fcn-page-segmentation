Fixed rotation in training sample generation and improved visualizations

callbacks.py
@@ -13,6 +13,7 @@ from evaluation.evaluations import score
 from evaluation.evaluations import CachedMetrics
 import psutil
 import shutil
+import os
 #from keras_visualize_activations.read_activations import *
 
 # Pixels per second logging too!!!
@@ -82,11 +83,12 @@ def visualize_weights(model, screen_height, screen_width, batch_num=0):
 
 import visuals
 class DisplayTrainingSamplesCallback(Callback):
-    def __init__(self, training_generator_class, model=None, interval=10):
+    def __init__(self, training_generator_class, model=None, interval=10, log_dir=None):
         self.training_generator = training_generator_class
         self.interval = interval
         self.batch_num = -2
         self.model = model
+        self.log_dir = log_dir
 
     def on_batch_end(self, batch, logs={}):
         self.batch_num += 1
@@ -96,9 +98,13 @@ class DisplayTrainingSamplesCallback(Callback):
                 plt.figure(3)
                 v = show(self.training_generator.gt, bgr=True)
                 plt.imshow(v)
+                if self.log_dir is not None:
+                    plt.savefig(os.path.join(self.log_dir, "TrainGT.png"))
             if self.training_generator.image is not None:
                 plt.figure(2)
                 plt.imshow(self.training_generator.image)
+                if self.log_dir is not None:
+                    plt.savefig(os.path.join(self.log_dir, "TrainImage.png"))
                 if self.model is not None:
                     im = self.training_generator.image
                     plt.figure(4)
@@ -107,6 +113,8 @@ class DisplayTrainingSamplesCallback(Callback):
                     print "Pred shape:", self.pred.shape
                     print "Pred min/max:", np.min(self.pred), np.max(self.pred)
                     plt.imshow(show(self.pred, bgr=True))
+                    if self.log_dir is not None:
+                        plt.savefig(os.path.join(self.log_dir, "TrainPred.png"))
             plt.pause(0.001)
 
 #if (self.iteration+1)%self.display_interval == 0:
@@ -202,7 +210,7 @@ class ShowTestPredsCallback(Callback):
         #    show(batch_x[0], None, preds[0], 1024, 768, "", self.batch_num, "Test")
 
 class DisplayAccuracyCallback(Callback):
-    def __init__(self, model, generator=None, generator_class=None, training_generator_class=None, pixel_counts_by_class=defaultdict(lambda:0), eval_interval=50):
+    def __init__(self, model, generator=None, generator_class=None, training_generator_class=None, pixel_counts_by_class=defaultdict(lambda:0), eval_interval=50, log_dir="./"):
         self.generator = generator
         self.generator_class = generator_class
         self.eval_interval = eval_interval
@@ -231,6 +239,7 @@ class DisplayAccuracyCallback(Callback):
         self.training_generator_class = training_generator_class
         self.batch_num = 0
         self.best_macro_fscore = 0.0
+        self.log_dir = log_dir
         self.model = model
 
     # on_batch_end
@@ -268,6 +277,25 @@ class DisplayAccuracyCallback(Callback):
         impath = self.generator_class.last_path
         #if do_show:
         #    show(batch_x[0], batch_y[0], preds[0], self.h, self.w, impath, self.batch_num, do_show=False)
+
+        #if self.training_generator.gt is not None:
+        plt.figure(8)
+        v = show(batch_y[0], bgr=True)
+        plt.imshow(v)
+        #if self.training_generator.image is not None:
+        plt.figure(9)
+        im = batch_x[0]
+        plt.imshow(im)
+        #if self.model is not None:
+
+        plt.figure(10)
+        print "DisplayValidationSamplesCallback input shape", im.shape
+        #self.pred = self.model.predict(np.reshape(im, [1,im.shape[0],im.shape[1],im.shape[2]]))[0]
+        self.pred = preds[0]
+        print "Pred shape:", self.pred.shape
+        print "Pred min/max:", np.min(self.pred), np.max(self.pred)
+        plt.imshow(show(self.pred, bgr=True))
+        plt.pause(0.001)
 
         print("Computing precisions, recalls, etc.!")
 
@@ -316,7 +344,7 @@ class DisplayAccuracyCallback(Callback):
         precisions, recalls, accuracies, f_scores, tot_gt_mass, overall_correct = cm["precision"], cm["recall"], cm["accuracy"], cm["f1_score"], cm["gt_mass"], cm["true_positives"]
 
         #precisions, recalls, accuracies, f_scores, tot_gt_mass, overall_correct = score(batch_y, preds)
-        
+
         print precisions.shape, recalls.shape, accuracies.shape, f_scores.shape, tot_gt_mass.shape, overall_correct.shape
         if self.training_generator_class is not None:
             self.training_generator_class.class_fscores = f_scores
@@ -435,7 +463,7 @@ class DisplayAccuracyCallback(Callback):
         plt.title('Metric Chart')
         plt.tight_layout()
         plt.pause(0.0001)
-        plt.savefig('LossMetrics.png')
+        plt.savefig(os.path.join(self.log_dir, 'LossMetrics.png'))
 
         print("========== Overall stats: =============")
         print("Average activation:", np.mean(preds), "Max activation:", np.max(preds), "Min activation:", np.min(preds))
@@ -462,7 +490,7 @@ class DisplayAccuracyCallback(Callback):
         plt.tight_layout()
         plt.title('Loss and Metric History')
         plt.pause(0.001)
-        plt.savefig('training.png')
+        plt.savefig(os.path.join(self.log_dir, 'training.png'))
         # Compute Precision, Recall, F-Score, and Pixel accuracy per class.
 
         mem = psutil.virtual_memory()
@@ -471,7 +499,7 @@ class DisplayAccuracyCallback(Callback):
         val_json = {"Loss": self.losses, "Precision":self.prec, "Recall": self.reca, "F-Score": self.fscore, "Accuracy": self.pixacc}
         #print(val_json)
         self.val_json_iters = val_json #.append(val_json)
-        with open("loss_history.json", 'w') as f:
+        with open(os.path.join(self.log_dir, "loss_history.json"), 'w') as f:
             json.dump(self.val_json_iters, f, indent=2)
 
         cv2.waitKey(10)
@@ -534,16 +562,40 @@ class LogTimingCallback(Callback):
         print("Epoch duration:", epoch_duration, "s")
         self.batches_elapsed = 0
 
+def _mkdir(newdir):
+    """works the way a good mkdir should :)
+        - already exists, silently complete
+        - regular file in the way, raise an exception
+        - parent directory(ies) does not exist, make them as well
+    """
+    if os.path.isdir(newdir):
+        pass
+    elif os.path.isfile(newdir):
+        raise OSError("a file with the same name as the desired " \
+                      "dir, '%s', already exists." % newdir)
+    else:
+        head, tail = os.path.split(newdir)
+        if head and not os.path.isdir(head):
+            _mkdir(head)
+        #print("_mkdir %s" % repr(newdir))
+        if tail:
+            os.mkdir(newdir)
+
+
 class TFModelSaverCallback(Callback):
     def __init__(self, model, model_save_path, save_interval=5000, start_iteration=0):
         self.model = model
         self.iteration = start_iteration
         self.model_save_path = model_save_path
         self.save_interval = save_interval
-        
+
     def on_batch_end(self, batch, logs={}):
+        self.iteration += 1
         if self.iteration > 0 and self.iteration % self.save_interval == 0:
             print "Saving model..."
+            dirname = os.path.dirname(self.model_save_path)
+            if not os.path.exists(dirname):
+                _mkdir(dirname)
             self.model.save(self.model_save_path)
 
 class TestModelCallback(Callback):

train.py
@@ -27,6 +27,25 @@ from models.model import build_model
 from predict import *
 #############################################################
 
+def _mkdir(newdir):
+    """works the way a good mkdir should :)
+        - already exists, silently complete
+        - regular file in the way, raise an exception
+        - parent directory(ies) does not exist, make them as well
+    """
+    if os.path.isdir(newdir):
+        pass
+    elif os.path.isfile(newdir):
+        raise OSError("a file with the same name as the desired " \
+                      "dir, '%s', already exists." % newdir)
+    else:
+        head, tail = os.path.split(newdir)
+        if head and not os.path.isdir(head):
+            _mkdir(head)
+        #print("_mkdir %s" % repr(newdir))
+        if tail:
+            os.mkdir(newdir)
+
 def train(args):
     batch_size = args.batch_size
     num_classes = args.num_classes
@@ -37,7 +56,7 @@ def train(args):
     model_save_path = args.model_save_path
     model_type = args.model_type
     ds_rate = 1.0
-    
+
     # Index the training set for efficient caching and batching, including class rebalancing.
     print "Training folder", training_folder
     index = index_training_set_by_class(training_folder, num_classes=num_classes)
@@ -47,10 +66,10 @@ def train(args):
     if args.batcher == 'simple':
         training_generator = training_generator_class.generate(in_width)
     elif args.batcher == 'mixed':
-        training_generator = mixed_get_batch([training_generator_class.legacy_get_batch(in_width), training_generator_class.generate(in_width)], [0.1, 0.9]) 
+        training_generator = mixed_get_batch([training_generator_class.legacy_get_batch(in_width), training_generator_class.generate(in_width)], [0.1, 0.9])
     else:
         training_generator = training_generator_class.legacy_get_batch(in_width)
-    
+
     # Optionally include a validation set for display, early stopping, and mini-validation during training.
     # If none is supplied, the training set will be used for real-time display, etc.
     print "Validation folder", validation_folder
@@ -62,8 +81,19 @@ def train(args):
     import psutil
     mem_orig = psutil.virtual_memory().used
     print("VIRTUAL MEMORY BEFORE MODEL CREATION:", mem_orig)
-    
+
+    if not os.path.exists(args.log_dir):
+        _mkdir(args.log_dir)
+
+    print ">>>> Log dir", args.log_dir, "Save path", args.model_save_path
+    if args.log_dir != os.path.dirname(args.model_save_path):
+        args.model_save_path = os.path.join(args.log_dir, args.model_save_path)
+        print "UPDATED", args.model_save_path
+        model_save_path = args.model_save_path
+
     # BUILD the appropriate model, depending on the framework and model construction parameters.
+    if args.framework.lower() == "keras" and not ".h5" in args.load_model_path[-3:]:
+        args.load_model_path = args.load_model_path + ".h5"
     model = build_model(args)
 
     # Now print memory stats, including estimated model size.
@@ -74,35 +104,37 @@ def train(args):
 
     # Print a model summary for debugging.
     print("Model Summary:", model.summary())
-    
+
     # Plot the model as a chart and save to a PNG file
     # TODO Extend TF and PyTorch models to do the same!
     if args.framework.lower() == "keras":
         from keras.utils import plot_model
-        plot_model(model, to_file='model.png')
+        plot_model(model, to_file=os.path.join(args.log_dir, 'model.png'))
 
     # CREATE callbacks.
     callbacks = []
 
     from callbacks import DisplayAccuracyCallback
-    callbacks.append(DisplayAccuracyCallback(model, validation_generator, validation_generator_class, training_generator_class=training_generator_class, pixel_counts_by_class=training_generator_class.dataset_sampler.pixel_counts_byclass, eval_interval=500))
+    callbacks.append(DisplayAccuracyCallback(model, validation_generator, validation_generator_class, training_generator_class=training_generator_class, pixel_counts_by_class=training_generator_class.dataset_sampler.pixel_counts_byclass, eval_interval=args.log_interval, log_dir=args.log_dir))
 
     from callbacks import DisplayTrainingSamplesCallback
-    callbacks.append(DisplayTrainingSamplesCallback(training_generator_class, model=model, interval=100))
+    callbacks.append(DisplayTrainingSamplesCallback(training_generator_class, model=model, interval=args.training_sample_visualization_interval, log_dir=args.log_dir))
 
     if args.framework.lower() == "keras":
+        if not ".h5" in model_save_path[-3:]:
+            model_save_path = model_save_path + ".h5"
         from keras.callbacks import ModelCheckpoint
         save_model_callback = ModelCheckpoint(filepath = model_save_path, verbose=1, save_best_only=True, period=1)
 
         from keras.callbacks import ReduceLROnPlateau
         callbacks.append(ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, verbose=0, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0.00000000001))
-    
+
         #callbacks.append(keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=0, mode='auto'))
     else:
         from callbacks import TFModelSaverCallback
-        save_model_callback = TFModelSaverCallback(model_save_path, model.sess)
+        save_model_callback = TFModelSaverCallback(model, model_save_path, args.model_save_interval)
     callbacks.append(save_model_callback)
-    
+
     from callbacks import LogTimingCallback
     callbacks.append(LogTimingCallback(batch_size))
 
@@ -144,7 +176,7 @@ A Trainer consumes a training dataset of the following format:
 "class_labels": {0: "dotted lines", 1: "handwriting", 2: "machine print", 3: "solid lines", 4: "stamps"}
 "abbrev_class_labels": [0: "DL", 1: "HW", 2: "MP", 3: "LN", 4: "ST"]
 "training_instance_paths":
-    [  
+    [
     "path1",
     "path2",
     ...
@@ -171,4 +203,3 @@ if __name__ == "__main__":
     import numpy as np
     np.random.seed(args.seed)  # for reproducibility
     train(args)
-